51ecd9eb886bac8e47e2d3a725bbfb5a41e355dd
israel <israel.bejarano.a@gmail.com>
2019-02-13 22:59:05 +0100

    do search in all collections with promises

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -2,27 +2,65 @@ var express = require('express');
 var app = express();
 var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 app.get('/todo/:busqueda', (req, res, next) => {
 
     var busqueda = req.params.busqueda;
     var regex = new RegExp(busqueda, 'i'); // creo una expresion regular para hacer el termino de busqueda insenible a mayus/minus
 
-    buscarHospitales(busqueda, regex).then(hospitales => {
+    Promise.all([
+        buscarHospitales(busqueda, regex),
+        buscarMedicos(busqueda, regex),
+        buscarUsuarios(busqueda, regex)
+    ]).then(respuestas => {
         res.status(200).json({
             ok: true,
-            hospitales: hospitales
+            hospitales: respuestas[0],
+            medicos: respuestas[1],
+            usuarios: respuestas[2]
         });
     });
 });
 
 function buscarHospitales(busqueda, regex) {
     return new Promise((resolve, reject) => {
-        Hospital.find({ nombre: regex }, (err, hospitales) => {
+        Hospital.find({ nombre: regex })
+            .populate('usuario', 'nombre email')
+            .exec((err, hospitales) => {
+                if (err) {
+                    reject('Error al cargar hospitales ', err);
+                } else {
+                    resolve(hospitales);
+                }
+            });
+    });
+
+}
+
+function buscarMedicos(busqueda, regex) {
+    return new Promise((resolve, reject) => {
+        Medico.find({ nombre: regex })
+            .populate('usuario', 'nombre email')
+            .populate('hospital')
+            .exec((err, medicos) => {
+                if (err) {
+                    reject('Error al cargar medicos ', err);
+                } else {
+                    resolve(medicos);
+                }
+            });
+    });
+
+}
+
+function buscarUsuarios(busqueda, regex) {
+    return new Promise((resolve, reject) => {
+        Usuario.find({}, 'nombre email role').or([{ 'nombre': regex }, { 'email': regex }]).exec((err, usuarios) => {
             if (err) {
-                reject('Error al cargar hospitales ', err);
+                reject('Error al cargar usuarios ', err);
             } else {
-                resolve(hospitales);
+                resolve(usuarios);
             }
         });
     });
