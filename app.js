fb51072c38f1d4491f3e32effc2c4857354f2557
israel <israel.bejarano.a@gmail.com>
2019-02-15 17:40:28 +0100

    get images by name from file system

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 // Conexion a BBDD
@@ -39,6 +40,7 @@ app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/busqueda', busquedaRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/', appRoutes); // esta ruta siempre tiene que ser la ultima si no no haria distinciones
 
 // Escuchas
