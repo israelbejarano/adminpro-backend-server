d1c70e589e27e2dc5e1ccb24875dc9490cf7abee
israel <israel.bejarano.a@gmail.com>
2019-02-23 11:36:33 +0100

    add find by id search in hospital s

--- routes/hospital.js
+++ routes/hospital.js
@@ -136,4 +136,33 @@ app.delete('/:id', mdAutenticacion.verificarToken, (req, res) => {
 
     });
 });
+
+// ========================================
+// obtener hospital por ID
+// ========================================
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Hospital.findById(id).populate('usuario', 'nombre img email') // con esto obtengo el objeto usuario entero asociado a este find y solo muestro id, nombre y email
+        .exec((err, hospital) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargando hospital de BBDD',
+                    errors: err
+                });
+            }
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El hospital con el id ' + id + ' no existe',
+                    errors: { message: 'No existe un hospital con ese ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                hospital: hospital
+            });
+        });
+});
+
 module.exports = app; //con esto puedo usar este fichero en cualquier parte del server
